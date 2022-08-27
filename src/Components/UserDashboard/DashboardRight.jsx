47f9ffdbe3a3db70a8d244319b6fa7e3700eb8c8
iinaamasum <iinaamasum@gmail.com>
2022-08-27 19:48:08 +0600

    grid center

--- src/Components/UserDashboard/DashboardRight.jsx
+++ src/Components/UserDashboard/DashboardRight.jsx
@@ -80,10 +80,12 @@ const DashboardRight = () => {
           </ul>
         </div>
         {/* projects card  */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-4 mt-5">
-          {projectsCardData.map((data) => (
-            <UserProjectsCard key={data.id} data={data} />
-          ))}
+        <div className="flex justify-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-4 mt-5">
+            {projectsCardData.map((data) => (
+              <UserProjectsCard key={data.id} data={data} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
