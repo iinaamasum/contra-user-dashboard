d3a6428ed9df3a3a6339bea868ba2c916c9133b7
iinaamasum <iinaamasum@gmail.com>
2022-08-27 06:09:25 +0600

    projects card rendered with default data

--- src/Components/UserDashboard/DashboardRight.jsx
+++ src/Components/UserDashboard/DashboardRight.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { AiOutlineEyeInvisible } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import pro1 from '../../Assets/image/pro1.jpeg';
+import pro2 from '../../Assets/image/pro2.jpeg';
+import pro3 from '../../Assets/image/pro3.jpeg';
+import pro4 from '../../Assets/image/pro4.jpeg';
+import pro5 from '../../Assets/image/pro5.jpeg';
 import UserProjectsCard from './UserProjectsCard';
 
 const DashboardRight = () => {
@@ -8,6 +13,43 @@ const DashboardRight = () => {
     { id: 1, path: '/work-experience', title: 'Work Experience' },
     { id: 2, path: '/calender', title: 'Calender' },
     { id: 3, path: '/my-feed', title: 'My Feed' },
+  ];
+
+  const projectsCardData = [
+    {
+      id: 1,
+      title: 'WWF My Footprint App UX',
+      description:
+        'How might we make it a cultural norm to reduce your impact on nature?',
+      img: `${pro1}`,
+    },
+    {
+      id: 2,
+      title: 'M&S sees 100k app downloads after launching Advent',
+      description:
+        'The calendar features prizes such as a meet and greet with Gary Barlow',
+      img: `${pro2}`,
+    },
+    {
+      id: 3,
+      title: 'Land Rover Explore',
+      description:
+        'What if we could support the launch of a new category of phone?',
+      img: `${pro3}`,
+    },
+    {
+      id: 4,
+      title: 'We Find Venues’ bookings hit £7.5m after product design',
+      description: 'What if you could make finding event space better?',
+      img: `${pro4}`,
+    },
+    {
+      id: 5,
+      title: 'Improving Design Sprints with Data',
+      description:
+        'How might we make it a cultural norm to reduce your impact on nature?',
+      img: `${pro5}`,
+    },
   ];
   return (
     <div className="w-[750px] py-[30px] mt-[70px] flex justify-center rounded-2xl">
@@ -48,7 +90,11 @@ const DashboardRight = () => {
           </p>
         </div>
         {/* projects card  */}
-        <UserProjectsCard />
+        <div className="">
+          {projectsCardData.map((data) => (
+            <UserProjectsCard key={data.id} data={data} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/Components/UserDashboard/UserProjectsCard.jsx
+++ src/Components/UserDashboard/UserProjectsCard.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import pro1 from '../../Assets/image/pro1.jpeg';
 
-const UserProjectsCard = () => {
+const UserProjectsCard = ({ data }) => {
+  const { title, description, img } = data;
+
   return (
     <div className="lg1200:flex items-start justify-between p-[30px] bg-[#fff] my-[30px] rounded-2xl gap-x-[20px]">
       <div className="h-[190px] w-full lg1200:w-[250px]">
-        <img className="h-full w-full rounded-xl" src={pro1} alt="" />
+        <img className="h-full w-full rounded-xl" src={img} alt="" />
       </div>
-      <div className="">
+      <div className="w-full lg1200:w-[415px]">
         <h2
           style={{
             lineHeight: '2.5rem',
           }}
-          className="text-2xl lg1200:text-4xl font-bold mb-3"
+          className="text-2xl lg1200:text-4xl font-bold mb-4"
         >
-          WWF My Footprint App UX
+          {title.slice(0, 45)}
         </h2>
-        <p>
-          How might we make it a cultural norm to reduce your impact on nature?
-        </p>
+        <p>{description}</p>
       </div>
     </div>
   );
